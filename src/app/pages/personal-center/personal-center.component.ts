f9aa8e4743ccdaba2cbdd5dba3920bff280dc767
guodonglin <guodonglin@dataocean.com.cn>
2019-01-07 01:18:47 +0800

    对接个人中心

--- src/app/pages/personal-center/personal-center.component.ts
+++ src/app/pages/personal-center/personal-center.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TokenService } from '../../shared/services/token.service';
 import * as jQuery from 'jquery';
-import { LinkService } from '../../shared/services/link.service';
 
 @Component({
   selector: 'app-personal-center',
@@ -14,19 +13,20 @@ export class PersonalCenterComponent implements OnInit {
   public ZH_TITLE: string;
   public EN_TITLE: string;
   public NAV_INDEX: number;
-  public mobile: string;
+  public allMes = {
+    user: {}
+  };
   public name: string;
   public company: string;
+  public selectedCompanyId: string;
   public position: string;
   public isNameReadonly = true;
-  public isCompanyReadonly = true;
   public isPositionReadonly = true;
+  public ifShowSubmitBtn = false;
+  public isEdits = [false, false, false];
+  public companys: any;
 
-  public ifNameResetSureClicked = false;
-  public ifComponyResetSureClicked = false;
-  public ifPositionResetSureClicked = false;
-
-  constructor(private http: HttpClient, private token: TokenService, private link: LinkService) {
+  constructor(private http: HttpClient, private token: TokenService) {
   }
 
   ngOnInit() {
@@ -34,44 +34,44 @@ export class PersonalCenterComponent implements OnInit {
     this.ZH_TITLE = '个人中心';
     this.EN_TITLE = 'PERSONAL CENTER';
     this.NAV_INDEX = 4;
-    this.getPersonalMes();
+    // 获取所有公司
+    this.http.get(
+      '/ucenter/rest/v2/services/ucenter_CompanyService/getAllCompany',
+      { headers: { 'Authorization': `Bearer ${this.token._token}` } }
+    ).subscribe(
+      data => {
+        this.companys = data;
+        this.getPersonalMes();
+      });
   }
 
-  onClickResetName(type) {
-    this.isNameReadonly = !this.isNameReadonly;
+  onClickResetName() {
+    this.isNameReadonly = false;
+    this.isEdits[0] = true;
+    this.ifShowSubmitBtn = true;
     jQuery('#name').focus();
-    if (!this.isNameReadonly) {
-      this.ifNameResetSureClicked = true;
-    } else {
-      this.sendResetMessage(type);
-    }
   }
 
-  onClickResetCompany(type) {
-    this.isCompanyReadonly = !this.isCompanyReadonly;
+  onClickResetCompany() {
+    this.isEdits[1] = true;
+    this.ifShowSubmitBtn = true;
     jQuery('#company').focus();
-    if (!this.isCompanyReadonly) {
-      this.ifComponyResetSureClicked = true;
-    } else {
-      this.sendResetMessage(type);
-    }
-  }
-  onClickResetPosition(type) {
-    this.isPositionReadonly = !this.isPositionReadonly;
-    jQuery('#position').focus();
-    if (!this.isPositionReadonly) {
-      this.ifPositionResetSureClicked = true;
-    } else {
-      this.sendResetMessage(type);
-    }
   }
 
-  sendResetMessage(type) {
+  onClickResetPosition() {
+    this.isPositionReadonly = false;
+    this.isEdits[2] = true;
+    this.ifShowSubmitBtn = true;
+    jQuery('#position').focus();
+  }
+
+  // 修改信息提交
+  onResetMesSubmit() {
     this.http.post(
       '/ucenter/rest/v2/services/ucenter_UserService/updateClient2',
       {
         'name': this.name,
-        'companyId': '7265364b-b0c5-e8bc-e927-7a3982458fd6',
+        'companyId': this.selectedCompanyId,
         'position': this.position
       },
       {
@@ -88,25 +88,26 @@ export class PersonalCenterComponent implements OnInit {
     );
   }
 
+  // 获取用户信息
   getPersonalMes() {
-    this.http.get(
+    this.http.get<any>(
       '/ucenter/rest/v2/services/ucenter_UserService/getCurrentClient',
       { headers: { 'Authorization': `Bearer ${this.token._token}` } }
     ).subscribe(
       data => {
-        this.mobile = data['mobile'];
+        this.allMes = data;
         this.name = data['name'];
         this.company = data['company'];
         this.position = data['user']['position'];
+        this.companys.forEach(element => {
+          if (element.name === this.company) {
+            this.selectedCompanyId = element.id;
+          }
+        });
         this.isNameReadonly = true;
-        this.isCompanyReadonly = true;
         this.isPositionReadonly = true;
-        this.ifNameResetSureClicked = false;
-        this.ifComponyResetSureClicked = false;
-        this.ifPositionResetSureClicked = false;
-        jQuery('#name').blur();
-        jQuery('#company').blur();
-        jQuery('#position').blur();
+        this.isEdits.fill(false);
+        this.ifShowSubmitBtn = false;
       });
   }
 
